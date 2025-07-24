3b0eb788da55ca3e4b3f02774f60738997ad2843
Cong J <jincong0523@outlook.com>
2025-07-25 00:58:32 +0200

    feat: react flow ui start up

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,100 @@
-import { Button } from "@/components/ui/button"
-
+import '@xyflow/react/dist/style.css';
+ 
+import {
+  ReactFlow,
+  type OnConnect,
+  Position,
+  useNodesState,
+  useEdgesState,
+  addEdge,
+  type Edge,
+  type Node,
+} from '@xyflow/react';
+ 
+import { NumNode } from '@/components/nodes/num-node';
+import { SumNode } from '@/components/nodes/sum-node';
+ 
+import { DataEdge } from '@/components/data-edge';
+import { useCallback } from 'react';
+ 
+const nodeTypes = {
+  num: NumNode,
+  sum: SumNode,
+};
+ 
+const initialNodes: Node[] = [
+  { id: 'a', type: 'num', data: { value: 0 }, position: { x: 0, y: 0 } },
+  { id: 'b', type: 'num', data: { value: 0 }, position: { x: 0, y: 200 } },
+  { id: 'c', type: 'sum', data: { value: 0 }, position: { x: 300, y: 100 } },
+  { id: 'd', type: 'num', data: { value: 0 }, position: { x: 0, y: 400 } },
+  { id: 'e', type: 'sum', data: { value: 0 }, position: { x: 600, y: 400 } },
+];
+ 
+const edgeTypes = {
+  data: DataEdge,
+};
+ 
+const initialEdges: Edge[] = [
+  {
+    id: 'a->c',
+    type: 'data',
+    data: { key: 'value' },
+    source: 'a',
+    target: 'c',
+    targetHandle: 'x',
+  },
+  {
+    id: 'b->c',
+    type: 'data',
+    data: { key: 'value' },
+    source: 'b',
+    target: 'c',
+    targetHandle: 'y',
+  },
+  {
+    id: 'c->e',
+    type: 'data',
+    data: { key: 'value' },
+    source: 'c',
+    target: 'e',
+    targetHandle: 'x',
+  },
+  {
+    id: 'd->e',
+    type: 'data',
+    data: { key: 'value' },
+    source: 'd',
+    target: 'e',
+    targetHandle: 'y',
+  },
+];
+ 
 function App() {
+  const [nodes, , onNodesChange] = useNodesState(initialNodes);
+  const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
+ 
+  const onConnect: OnConnect = useCallback(
+    (params) => {
+      setEdges((edges) =>
+        addEdge({ type: 'data', data: { key: 'value' }, ...params }, edges),
+      );
+    },
+    [setEdges],
+  );
+ 
   return (
-    <div className="flex min-h-svh flex-col items-center justify-center">
-      <Button variant="default">Click me</Button>
+    <div className="h-screen w-screen p-8">
+      <ReactFlow
+        nodes={nodes}
+        edges={edges}
+        onNodesChange={onNodesChange}
+        onEdgesChange={onEdgesChange}
+        onConnect={onConnect}
+        nodeTypes={nodeTypes}
+        edgeTypes={edgeTypes}
+        fitView
+      />
     </div>
-  )
+  );
 }
-
-export default App
+export default App;
