45aeab2749ecfd39c5a04c5352b091e699afe931
Cong J <jincong0523@outlook.com>
2025-07-27 02:56:49 +0200

    feat: add select to guild word node

--- src/components/nodes/guideword-node.tsx
+++ src/components/nodes/guideword-node.tsx
@@ -6,8 +6,15 @@ import {
 } from "@/components/base-node";
 import { Quote } from "lucide-react";
 import { type Node, type NodeProps, Position, useReactFlow } from '@xyflow/react';
-import { EditableText } from './subComponents/editable-text';
+import { EditableText } from '@/components/nodes//subComponents/editable-text';
 import { NodeHeader } from "@/components/nodes/subComponents/node-header";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select"
 
 export type GuideWordNode = Node<{
   content: string;
@@ -23,7 +30,7 @@ export function GuideWordNode({ id, data }: NodeProps<GuideWordNode>) {
     setNodes((nodes) => nodes.filter((node) => node.id !== id));
   }, [id, setNodes]);
   
-  const [content, setContent] = useState(data.content);
+  const [content, setContent] = useState(data.content || "no");
 
   return (
     <BaseNode className="w-40 border-fuchsia-200 bg-fuchsia-50">
@@ -36,10 +43,16 @@ export function GuideWordNode({ id, data }: NodeProps<GuideWordNode>) {
       />
       <BaseHandle id={`${id}-target`} type="target" position={Position.Top} className="nodrag" /> 
       <BaseNodeContent>
-        <EditableText
-          content={content}
-          onChange={(value) => setContent(value)}
-        /> 
+        <Select value={content} onValueChange={(value) => setContent(value)}>
+          <SelectTrigger>
+            <SelectValue placeholder="Select a guide word" />
+          </SelectTrigger>
+          <SelectContent>
+            <SelectItem value="no">no</SelectItem>
+            <SelectItem value="other than">other than</SelectItem>
+            <SelectItem value="part of">part of</SelectItem>
+          </SelectContent>
+        </Select>
       </BaseNodeContent>
       <BaseHandle id={`${id}-source`} type="source" position={Position.Bottom} className="nodrag" />
     </BaseNode>
